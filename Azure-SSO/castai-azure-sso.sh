f9bdaa43a5a4308efa31a13a2ed702deec2e695a
AdminAV <AdminAV@ardaghlzpoc.onmicrosoft.com>
2023-06-06 15:47:33 -0700

    base64 encode and single-tenant

--- Azure-SSO/castai-azure-sso.sh
+++ Azure-SSO/castai-azure-sso.sh
@@ -3,14 +3,14 @@ appName="castai-azuresso"
 redirectUri="https://login.cast.ai/login/callback"
 
 # Create enterprise app registration
-app=$(az ad app create --display-name $appName --web-redirect-uris $redirectUri --query "{appId: appId}" -o tsv)
+app=$(az ad app create --display-name $appName --web-redirect-uris $redirectUri --sign-in-audience AzureADMyOrg --query "{appId: appId}" -o tsv)
 
 # Create a password (client secret) for the app
 password=$(az ad app credential reset --id $app --query password -o tsv)
 
 # Output the client ID and client secret
-echo "Client ID: $app" > credentials.txt
-echo "Client Secret: $password"	>> credentials.txt
+echo "Client ID: $app" | base64 > credentials.txt
+echo "Client Secret: $password" | base64 >> credentials.txt
 
 # Display success message
 echo "Enterprise app registration created successfully. Client ID and Client Secret saved to credentials.txt file."
